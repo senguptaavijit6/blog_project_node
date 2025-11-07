682de2458fb458fc90eab1976e7f928945c68a38
senguptaavijit6 <71085726+senguptaavijit6@users.noreply.github.com>
2025-11-07 12:21:18 +0530

    Log SAMPLE_ID in connectDB method for testing env
    Log SAMPLE_ID from environment variables during DB connection.

--- app/config/db.js
+++ app/config/db.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose")
 
 class DB {
     async connectDB() {
+        console.log(process.env.SAMPLE_ID)
         try {
             await mongoose.connect(process.env.DB_URI)
             console.log("Database connection successful");
